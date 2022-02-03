7a5d22d2533abcafc68fd3f7f67cde7978b450a0
Doric <doricwhite@gmail.com>
2022-02-03 11:22:53 -0500

    initial commit for sample timer using JS

---
+++ index.js
@@ -0,0 +1,23 @@
+"use strict";
+
+class Timer {
+  constructor(durationInput, startButton, pauseButton) {
+    this.durationInput = durationInput;
+    this.startButton = startButton;
+    this.pauseButton = pauseButton;
+
+    this.startButton.addEventListener("click", this.start);
+  }
+
+  start = () => {};
+
+  pause = () => {};
+
+  onDurationChange() {}
+}
+
+const durationInput = document.querySelector("#duration");
+const startButton = document.querySelector("#start");
+const pauseButton = document.querySelector("#pause");
+
+const timer = new Timer(durationInput, startButton, pauseButton);
