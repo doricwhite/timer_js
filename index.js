f59237e3940bc1ff5b9da5e6c06aadbddc1f9d8c
Doric <doricwhite@gmail.com>
2022-02-03 12:22:57 -0500

    update methods in timer class to include time functtionality

--- index.js
+++ index.js
@@ -6,12 +6,42 @@ class Timer {
     this.startButton = startButton;
     this.pauseButton = pauseButton;
 
+    // Event Listeners
     this.startButton.addEventListener("click", this.start);
+    this.pauseButton.addEventListener("click", this.pause);
   }
 
-  start = () => {};
+  // Start the time countdown
+  start = () => {
+    this.tick();
+    this.timerId = setInterval(this.tick, 1000);
+  };
 
-  pause = () => {};
+  // Decrements the time provided by the user
+  tick = () => {
+    // check to stop timer when it reaches 0
+    if (this.timeRemaining <= 0) {
+      this.pause();
+    } else {
+      //setter -------------------> getter
+      this.timeRemaining = this.timeRemaining - 1;
+    }
+  };
+
+  // Getter
+  get timeRemaining() {
+    return parseFloat(this.durationInput.value);
+  }
+
+  // Setter
+  set timeRemaining(time) {
+    this.durationInput.value = time;
+  }
+
+  // Pause time duration
+  pause = () => {
+    clearInterval(this.timerId);
+  };
 
   onDurationChange() {}
 }
